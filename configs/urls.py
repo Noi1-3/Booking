2f2dc503555c9b52c6fbe685de3f38b5e2b79eda
Noi_3 <orireg636@gmail.com>
2026-09-11 15:01:11 +0300

    changed UI + changed some parameters for UI

--- configs/urls.py
+++ configs/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/',    admin.site.urls),
-    path('users/',    include('users.urls')),
-    path('hotels/',   include('hotels.urls')),
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('hotels/', include('hotels.urls')),
     path('bookings/', include('bookings.urls')),
+
+
+    path('', RedirectView.as_view(url='/hotels/', permanent=True)),
 ]
